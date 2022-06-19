c1be607658953a102d305c39f6da7a58570ea5e7
Zhishi <whfuyn@outlook.com>
2022-06-19 14:34:47 +0800

    tss_segment and GDT

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,3 +1,4 @@
+use core::mem::size_of;
 use crate::lazy_static;
 use crate::x86_64::{PrivilegeLevel, SegmentSelector, VirtAddr};
 
@@ -18,9 +19,10 @@ lazy_static! {
     };
 
     static ref GDT: GlobalDescriptorTable = {
-        let gdt = GlobalDescriptorTable::new();
-        todo!()
-
+        let mut gdt = GlobalDescriptorTable::new();
+        gdt.add_entry(Descriptor::kernel_segment());
+        gdt.add_entry(Descriptor::tss_segment(&TSS));
+        gdt
     };
 }
 
@@ -51,7 +53,7 @@ impl TaskStateSegment {
         Self {
             privilege_stack_table: [VirtAddr::zero(); 3],
             interrupt_stack_table: [VirtAddr::zero(); 7],
-            iomap_base: core::mem::size_of::<TaskStateSegment>() as u16,
+            iomap_base: size_of::<TaskStateSegment>() as u16,
             reserved_1: 0,
             reserved_2: 0,
             reserved_3: 0,
@@ -145,7 +147,22 @@ impl Descriptor {
     }
 
     fn tss_segment(tss: &'static TaskStateSegment) -> Self {
+        use crate::bit_field::BitField;
+
         let ptr = tss as *const _ as u64;
-        todo!()
+        let mut low = DescriptorFlags::PRESENT;
+        // base
+        low.set_bits(16..40, ptr.get_bits(0..24));
+        low.set_bits(56..64, ptr.get_bits(24..32));
+        // TODO: what does those below comments mean?
+        // limit (the `-1` in needed since the bound is inclusive)
+        low.set_bits(0..16, (size_of::<TaskStateSegment>() - 1) as u64);
+        // type (0b1001 = available 64-bit tss)
+        low.set_bits(40..44, 0b1001);
+
+        let mut high = 0;
+        high.set_bits(0..32, ptr.get_bits(32..64));
+
+        Descriptor::SystemSegment(low, high)
     }
 }
